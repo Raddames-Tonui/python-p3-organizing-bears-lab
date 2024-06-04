9880aca6624d667158cdb415459a6d3faf39a253
Raddames <raddamestonui48@gmail.com>
2024-06-04 15:27:11 +0300

    completed assignment

---
+++ lib/create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS bears(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    age INTEGER,
+    sex CHAR(1),
+    color TEXT,
+    temperament TEXT,
+    alive BOOLEAN 
+);
+
